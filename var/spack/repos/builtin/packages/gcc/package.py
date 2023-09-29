4c43485c7483f34f588b43c40cfd1b0055652679
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-09-29 16:31:46 -0400

    fix(gcc): location where llvm utils are copied

--- var/spack/repos/builtin/packages/gcc/package.py
+++ var/spack/repos/builtin/packages/gcc/package.py
@@ -1006,7 +1006,7 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage):
 
         # Copy LLVM utils
         llvm_bin_path = join_path(spec["llvm"].prefix, "bin")
-        llvm_util_path = join_path(prefix, "libexec", "gcc", "amdgcn-amdhsa")
+        llvm_util_path = join_path(prefix, "amdgcn-amdhsa", "bin")
         mkdirp(llvm_util_path)
         copy(
             "{0}".format(join_path(llvm_bin_path, "llvm-ar")),
